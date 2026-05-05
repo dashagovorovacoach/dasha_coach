790b73935990455c7a1ecb47d23cba7c7c911c88
Ivan Tiutin <tyutin.iv95@gmail.com>
2026-05-05 21:30:40 +0800

    Fixed info cards and added animation for faq section

--- script.js
+++ script.js
@@ -1,3 +1,87 @@
+class Accordion {
+    constructor(el) {
+        this.el = el;
+        this.summary = el.querySelector("summary");
+        this.content = el.querySelector(".content");
+
+        this.animation = null;
+        this.isClosing = false;
+        this.isExpanding = false;
+
+        this.summary.addEventListener("click", (e) => this.onClick(e));
+    }
+
+    onClick(e) {
+        e.preventDefault();
+        this.el.style.overflow = "hidden";
+        if (this.isClosing || !this.el.open) {
+            this.open();
+        } else if (this.isExpanding || this.el.open) {
+            this.shrink();
+        }
+    }
+
+    shrink() {
+        this.isClosing = true;
+
+        const startHeight = `${this.el.offsetHeight}px`;
+        const endHeight = `${this.summary.offsetHeight}px`;
+
+        if (this.animation) {
+            this.animation.cancel();
+        }
+
+        this.animation = this.el.animate(
+            {
+                height: [startHeight, endHeight],
+            },
+            {
+                duration: 300,
+                easing: "ease-out",
+            },
+        );
+
+        this.animation.onfinish = () => this.onAnimationFinish(false);
+        this.animation.oncancel = () => (this.isClosing = false);
+    }
+
+    open() {
+        this.el.style.height = `${this.el.offsetHeight}px`;
+        this.el.open = true;
+        window.requestAnimationFrame(() => this.expand());
+    }
+
+    expand() {
+        this.isExpanding = true;
+        const startHeight = `${this.el.offsetHeight}px`;
+        const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+
+        if (this.animation) {
+            this.animation.cancel();
+        }
+
+        this.animation = this.el.animate(
+            {
+                height: [startHeight, endHeight],
+            },
+            {
+                duration: 300,
+                easing: "ease-out",
+            },
+        );
+        this.animation.onfinish = () => this.onAnimationFinish(true);
+        this.animation.oncancel = () => (this.isExpanding = false);
+    }
+
+    onAnimationFinish(open) {
+        this.el.open = open;
+        this.animation = null;
+        this.isClosing = false;
+        this.isExpanding = false;
+        this.el.style.height = this.el.style.overflow = "";
+    }
+}
+
 const menuToggle = document.querySelector(".menu-toggle");
 const mobileMenu = document.querySelector(".mobile-menu");
 const mobileMenuClose = document.querySelector(".mobile-menu__close");
@@ -28,11 +112,4 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
-faqItems.forEach((item) => {
-    item.addEventListener("toggle", () => {
-        if (!item.open) return;
-        faqItems.forEach((other) => {
-            if (other !== item) other.open = false;
-        });
-    });
-});
+faqItems.forEach((item) => new Accordion(item));
